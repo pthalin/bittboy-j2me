f87d38efa1f9f50c246bbeea02530f53b93f96c1
Patrik Thalin <patrik@thalin.se>
2020-06-10 23:25:37 +0200

    Add icon

--- release.sh
+++ release.sh
@@ -25,6 +25,7 @@ done
 cp -r midpath/bin/ release/bittboy-j2me/
 cp -r midpath/configuration/ release/bittboy-j2me/
 cp ./bittboy-j2me release/menu
+cp ./icon.png release/bittboy-j2me/
 cp ./INSTALL.txt release
 cd release
-zip bitboy-j2me.zip -r *
+zip bittboy-j2me.zip -r *
